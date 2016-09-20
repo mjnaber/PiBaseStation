5c2eeec9b1c824b05d857f5f21b10da6dc58860f
Mark Neal <mjn@aber.ac.uk>
2016-09-20 17:19:24 +0000

    Basic changes to sink.c

--- sink.c
+++ sink.c
@@ -10,93 +10,95 @@ int tty_fd;
 
 int open_moteino(void)
 {
-struct termios tio;
+	struct termios tio;
 
-   // Open the device
-   //int tty_fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-   tty_fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
-   
-   // Check if the port didn't open correctly
-   if (tty_fd==-1)
-   {
-      return 0;
-   }
+	// Open the device
+	//int tty_fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
+	tty_fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_NDELAY);
 
-   tio.c_iflag = 0;
-   tio.c_oflag = 0;
-   tio.c_cflag = CS8 | CREAD | CLOCAL;
-   tio.c_lflag = 0;
-   tio.c_cc[VMIN] = 0;
-   tio.c_cc[VTIME] = 0;
+	// Check if the port didn't open correctly
+	if (tty_fd==-1)
+	{
+		return -1;
+	}
 
-   if (cfsetospeed(&tio, B115200) || cfsetispeed(&tio, B115200))
-   {
-      return 0;
-   }
+	tio.c_iflag = 0;
+	tio.c_oflag = 0;
+	tio.c_cflag = CS8 | CREAD | CLOCAL;
+	tio.c_lflag = 0;
+	tio.c_cc[VMIN] = 0;
+	tio.c_cc[VTIME] = 0;
 
-   cfmakeraw(&tio);
-   tcflush(tty_fd, TCIFLUSH);
-   tcsetattr(tty_fd, TCSANOW, &tio);
+	if (cfsetospeed(&tio, B115200) || cfsetispeed(&tio, B115200))
+	{
+		return -1;
+	}
+
+	cfmakeraw(&tio);
+	tcflush(tty_fd, TCIFLUSH);
+	tcsetattr(tty_fd, TCSANOW, &tio);
 
 }
- 
+
 main()
 {
-int n = 0;
-int i = 0;
-int pos = 0;
-int total = 0;
-char buf[1];
-CURL *curl;
-CURLcode res;
-time_t result = time(NULL); 
-char webbuf [4096];
-char finalbuf [4096];
-curl = curl_easy_init();
+	int n = 0;
+	int i = 0;
+	int pos = 0;
+	int total = 0;
+	char buf[1];
+	CURL *curl;
+	CURLcode res;
+	time_t result = time(NULL); 
+	char webbuf [4096];
+	char finalbuf [4096];
+	curl = curl_easy_init();
 
- for (i = 0; i < sizeof(finalbuf); i++)
- {
-   	finalbuf[i] = 0;
-   	webbuf[i] = 0;
- }
-
- if(curl) {
-    curl = curl_easy_init();
-    curl_easy_setopt(curl, CURLOPT_URL, "http://users.aber.ac.uk/mjn/recorddata.php");
-    /* First set the URL that is about to receive our POST. This URL can
-       just as well be a https:// URL if that is what should receive the
-       data. */
-    while (1)
-    {
-    	while(buf[0] != '\n')
+	if (-1 == open_moteino())
 	{
-   		n = read (tty_fd, buf, 1); 
-		if (n > 0)
-		{
-      			webbuf[pos++] = buf[0];
-    		}
-		n = 0;
+		printf("Failed to open Moteino!");
+		return -1;
 	}
-        buf[0] = 0;
-	total += pos;
-	if (total > 10)
-	{
-		result = time(NULL); 
-		sprintf(finalbuf, "data= %s %s", asctime(gmtime(&result)), webbuf);
-    		/* Now specify the POST data */
-		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, finalbuf);
-		printf(finalbuf);
 
-    		/* Perform the request, res will get the return code */
-    		res = curl_easy_perform(curl);
-                total = 0;
-		for (i = 0; i < sizeof(finalbuf); i++)
+	for (i = 0; i < sizeof(finalbuf); i++)
+	{
+		finalbuf[i] = 0;
+		webbuf[i] = 0;
+	}
+
+	curl = curl_easy_init();
+	curl_easy_setopt(curl, CURLOPT_URL, "http://users.aber.ac.uk/mjn/recorddata.php");
+
+	while (1)
+	{
+		while(buf[0] != '\n')
 		{
-			webbuf[i] = 0;
-			finalbuf[i] = 0;
+			n = read (tty_fd, buf, 1); 
+			if (n > 0)
+			{
+				webbuf[pos++] = buf[0];
+			}
+			n = 0;
 		}
-		pos = 0;
+		buf[0] = 0;
+		total += pos;
+		if (total > 10)
+		{
+			result = time(NULL); 
+			sprintf(finalbuf, "data= %s %s", asctime(gmtime(&result)), webbuf);
+			/* Now specify the POST data */
+			curl_easy_setopt(curl, CURLOPT_POSTFIELDS, finalbuf);
+			printf(finalbuf);
+
+			/* Perform the request, res will get the return code */
+			res = curl_easy_perform(curl);
+			total = 0;
+			for (i = 0; i < sizeof(finalbuf); i++)
+			{
+				webbuf[i] = 0;
+				finalbuf[i] = 0;
+			}
+			pos = 0;
+		}
 	}
-    }
- }
 }
